911544fd6ba21137a435ebb6ca558d461e456dd0
Pika-Pika27 <itsmepriyanka27@gmail.com>
2025-12-18 20:34:50 +0545

    Add files via upload

---
+++ processor.py
@@ -0,0 +1,46 @@
+def process_setopati(soup):
+    content_box = soup.find('div', class_='editor-box')
+    txt=content_box.text
+    return txt
+
+
+def process_ratopati(soup):
+    text_list=soup.find('div', class_='news-contentarea').find_all('p')
+    c=[cnt.text for cnt in text_list]
+    tx=' '.join(c)
+    return tx
+
+
+def process_ekantipur(soup): 
+    news_block=soup.find('div', class_='col-xs-12 col-sm-12 col-md-12')
+    text=news_block.text
+    return text
+
+    
+
+def process_nagariknews(soup):
+    content=soup.find('div', class_="col-lg-9 pl-md-4 pr-md-5")
+    cnt=content.text
+    return cnt
+
+
+def process_onlinekhabar(soup):
+    content = soup.find('div', class_='ok18-single-post-content-wrap')
+    text_data=content.text.strip().encode('latin1').decode('utf-8')
+    return text_data
+
+
+def process_nayapatrika(soup):
+    content = soup.find('div', class_='news-details-content')
+    ny=content.text
+    return ny
+
+
+def process_himalkhabar(soup):
+    content = soup.find('div',class_='')
+    hk=content.text
+    return hk
+
+
+def process_gorkhapatra(soup):
+    content = soup.find('div',class_=)
